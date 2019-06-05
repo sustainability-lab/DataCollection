0affadfe17d1165289e8c001ed3fe55fbb8a503a
sumitkanu <35460954+sumitkanu@users.noreply.github.com>
2019-06-05 17:47:01 +0530

    Update Client.ino

--- Air_Quality_data_Collection/Client.ino
+++ Air_Quality_data_Collection/Client.ino
@@ -21,7 +21,10 @@ Adafruit_CCS811 ccs;
 void setup() {
   Serial.begin(9600);
    pinMode(pwmPin, INPUT);
-  WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
+   pinMode(12, OUTPUT);
+   pinMode(15, OUTPUT);
+   pinMode(13, OUTPUT);
+WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
   WiFi.mode(WIFI_STA);        //This line hides the viewing of ESP as wifi hotspot
   WiFi.begin(ssid, password);     //Connect to your WiFi router
   Serial.println("");
@@ -57,6 +60,7 @@ void setup() {
 void loop() {
   HTTPClient http;  ;//Declare object of class HTTPClient
  if(ccs.available()){
+  digitalWrite(12,HIGH);
     float temp = ccs.calculateTemperature();
 //      Serial.println(temp);
     if(!ccs.readData()){
@@ -72,6 +76,9 @@ void loop() {
       while(1);
     }
   }
+  delay(100);
+  digitalWrite(12,LOW);
+  digitalWrite(15,HIGH);
       do {
 
         th = pulseIn(pwmPin, HIGH, 1004000)/1000; 
@@ -80,14 +87,16 @@ void loop() {
 
         ppm2 = 5000 * (th-2)/(th+tl-4);
 
-    } while (ppm2 < 0.0); 
+    } while (ppm2 < 0.0);
+    digitalWrite(15,LOW); 
   //Post Data
+  digitalWrite(13,HIGH);
   postData =ppm ;
   postData = postData +","+ppm2;
   Serial.println(postData);
   
   
-  http.begin("http://10.0.71.32:5005/");              //Specify request destination
+  http.begin("http://10.0.137.171:5005/");              //Specify request destination//10.0.71.32
   //http.addHeader("Content-Type", "application/x-www-form-urlencoded");    //Specify content-type header
  
   int httpCode = http.POST(postData);   //Send the request
@@ -97,6 +106,7 @@ void loop() {
   Serial.println(payload);    //Print request response payload
  
   http.end();  //Close connection
-  
+  delay(100);
+  digitalWrite(13,LOW);
   delay(5000);  //Post Data at every 5 seconds
 }
